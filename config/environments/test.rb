068949c3d0c47e50fc58dce6c1d0df7a77b77612
Ross Cooperman <cooperman@gmail.com>
2009-11-14 12:44:04 -0500

    frozen 2.3.4 version of rails

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -22,6 +22,9 @@ config.action_controller.allow_forgery_protection    = false
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
 
+config.gem "thoughtbot-shoulda",      :lib => "shoulda",      :source => "http://gems.github.com"
+config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
+
 # Use SQL instead of Active Record's schema dumper when creating the test database.
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
