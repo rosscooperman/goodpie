33b62c8bbe2e67594098dd2ee5b9851ec54862dc
Ross Cooperman <cooperman@gmail.com>
2009-11-15 10:56:58 -0500

    replaced rails default JS libraries (prototype) with JQuery

--- config/initializers/ensure_dj_runnning.rb
+++ config/initializers/ensure_dj_runnning.rb
@@ -1,19 +1,22 @@
-# determine the process status of the current delayed job worker
-DJ_PIDFILE = "#{RAILS_ROOT}/tmp/pids/dj.pid"
-system("[ -e #{DJ_PIDFILE} ] && ps -p `cat #{DJ_PIDFILE}` | grep -q ruby");
+# do not do any of this in the test environment
+unless RAILS_ENV == "test"
+  # determine the process status of the current delayed job worker
+  DJ_PIDFILE = "#{RAILS_ROOT}/tmp/pids/dj.pid"
+  system("[ -e #{DJ_PIDFILE} ] && ps -p `cat #{DJ_PIDFILE}` | grep -q Delayed");
 
-# if the process is not currently running, start a new one (this should pretty much happen
-# every time the server gets started but just in case, only one delayed job worker at a time)
-if $? != 0
-  pid = fork
-  if pid.nil?
-    $0 = 'Delayed job: worker'
-    Delayed::Worker.new.start
-    exit(0)
-  else
-    File.open(DJ_PIDFILE, "w+") do |file|
-      file.write pid
+  # if the process is not currently running, start a new one (this should pretty much happen
+  # every time the server gets started but just in case, only one delayed job worker at a time)
+  if $? != 0
+    pid = fork
+    if pid.nil?
+      $0 = 'Delayed job: worker'
+      Delayed::Worker.new.start
+      exit(0)
+    else
+      File.open(DJ_PIDFILE, "w+") do |file|
+        file.write pid
+      end
+      Process.detach(pid)
     end
-    Process.detach(pid)
   end
 end
