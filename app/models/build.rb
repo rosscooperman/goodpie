bb9b1abdd40fd0f940f358889382548d6c5840e9
Ross Cooperman <cooperman@gmail.com>
2009-11-15 00:29:58 -0500

    couple of small tweaks to build process.  works pretty smoothly now.

--- app/models/build.rb
+++ app/models/build.rb
@@ -2,12 +2,24 @@ class Build < ActiveRecord::Base
   belongs_to :project
 
   def go
+    exit_code = do_build
+    if exit_code == 0
+      project.update_attributes(:status => 'success')
+    else
+      project.update_attributes(:status => 'failed')
+    end
+    exit_code == 0
+  end
+
+  private
+
+  def do_build
     repo_dir = File.join(BUILD_DIRECTORY, self.project.repo_name)
     IO.popen("cd #{repo_dir} && #{self.project.steps.first.command}") do |io|
       until io.eof?
         io.readline
       end
     end
-    $? == 0
+    $?
   end
 end
