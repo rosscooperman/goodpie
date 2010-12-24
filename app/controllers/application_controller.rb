724ea4519a6a5a7a5a450487670520af4c6561e3
Micah Frost <micah.frost@fracturedatlas.org>
2010-12-23 19:04:00 -0500

    Fixed a few deprecation notices

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   helper :all
   protect_from_forgery
-  
-  # Scrub sensitive parameters from your log
-  filter_parameter_logging :output
 end
